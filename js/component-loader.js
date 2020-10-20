be6fed422802bb630841b344a8c2cd0e35fda364
Axel Roussille Åberg <axelra82@gmail.com>
2020-10-20 16:39:49 +0200

    Fix script load order issues

--- js/component-loader.js
+++ js/component-loader.js
@@ -1,47 +1,10 @@
+
 // This is just a clean up process to remove the inital script
-// in head, that createt the script element for this file
+// in head. Consider removing for production.
 const removeInitalScript = document.head.querySelector('script');
 removeInitalScript.parentNode.removeChild(removeInitalScript);
 
-// Quick test to see if we are on index page
-// We'll use this test later to handle the menu links
-const isIndex = window.location.pathname === '/' ? true : false;
-
-// Test if we are on CHAS Academy's GitHub pages
-const chasGitHub = 'chas-academy.github.io';
-const gitHubRepoName = 'u02-redesign-unit7';
-const isGitHub = window.location.host === chasGitHub ? true : false;
-
-// Create empty script element for menu toggler
-const menuTogglerScriptEl = document.createElement('script');
-
-// Insert menu toggler file in script source
-menuTogglerScriptEl.src = !isGitHub ? `/js/menu-toggle.js` : `/${gitHubRepoName}/js/menu-toggle.js`;
-
-// Append script to head (NOTE! NOT header element)
-document.head.appendChild(menuTogglerScriptEl);
-
-// Create empty script element for search
-const searchScripEl = document.createElement('script');
-
-// Insert menu toggler file in script source
-searchScripEl.src = !isGitHub ? `/js/search.js` : `/${gitHubRepoName}/js/search.js`;
-
-// Create empty script element for scroll to top
-const scrollTopScripEl = document.createElement('script');
-
-// Insert menu toggler file in script source
-scrollTopScripEl.src = !isGitHub ? `/js/scroll-to-top.js` : `/${gitHubRepoName}/js/scroll-to-top.js`;
-
-// Append scripts to body. Append, rather than prepend
-// will add script at the end of body since this file
-// must load last (otherwise the function can't find its elements
-// since they don't exist yet)
-document.body.appendChild(searchScripEl);
-document.body.appendChild(scrollTopScripEl);
-
-
-const componentLoader = () => {
+const componentLoader = async () => {
 
 	// Define keyword
 	const keyword = 'component';
@@ -49,98 +12,109 @@ const componentLoader = () => {
 	// Assign const componentElemets to all elements with keyword attribute on page
 	const componentElemets = document.querySelectorAll(`[${keyword}]`);
 
-	// Run all elements in for each loop
-	componentElemets.forEach(async (el) => {
+	// Run all elements in for loop NOTE: Do not use forEach!
+	// For each loop will not wait for all component elements
+	// We need to make sure that all component elements
+	// are in place before we can continue
+	for (let i = 0; i < componentElemets.length; i++) {
 
-		// Best practice for async is to run it in try/catch block
-		try {
-			// Get component tag name
-			const componentTag = el.tagName;
+		// Define el (const) from componentElemets
+		// using iterator index
+		const el = componentElemets[i];
+		const componentTag = el.tagName;
 
-			// Get file location from element atrribute
-			const file = !isGitHub ? el.getAttribute(keyword) : `/${gitHubRepoName}${el.getAttribute(keyword)}`;
+		// Get file location from element atrribute
+		const file = !isGitHub ? el.getAttribute(keyword) : `/${gitHubRepoName}${el.getAttribute(keyword)}`;
 
-			if (file) {
-				const fetchResponse = await fetch(file);
-				let html = fetchResponse.ok ? await fetchResponse.text() : false;
+		if (file) {
+			const fetchResponse = await fetch(file);
+			let html = fetchResponse.ok ? await fetchResponse.text() : false;
+
+			// For now we only need to change things in header element
+			if (componentTag === 'HEADER') {
 
 				// GitHub pages are on organization
-				// We need to fix menu anchors to match this
+				// We need to fix some src and href to match this
 				if (isGitHub) {
+					// Parse HTML string to DOM so we can manipulate it
+					const nodeList = new DOMParser().parseFromString(html, 'text/html').body;
 
-					// For now we only need to change things in header element
-					if (componentTag === 'HEADER') {
+					const logotypeLink = nodeList.querySelector('a#main-header-logotype');
+					logotypeLink.href = `/${gitHubRepoName}`;
 
-						const nodeList = new DOMParser().parseFromString(html, 'text/html').body;
+					const headerLogotype = nodeList.querySelector('a#main-header-logotype img');
+					headerLogotype.src = `/${gitHubRepoName}${headerLogotype.getAttribute('src')}`;
 
-						const headerLogotype = nodeList.querySelector('a#main-header-logotype img');
-						headerLogotype.src = `/${gitHubRepoName}${headerLogotype.getAttribute('src')}`;
+					// Get all anchors in list items from menu (nav)
+					const menuItems = nodeList.querySelectorAll('.container nav ul li a');
 
-						// Get all anchors in list items from menu (nav)
-						const menuItems = nodeList.querySelectorAll('.container nav ul li a');
+					// Loop anchors in for each loop
+					menuItems.forEach(item => {
 
-						// Loop anchors in for each loop
-						menuItems.forEach(item => {
+						// Get item href
+						const itemUrl = item.href;
 
-							// Get item href
-							const itemUrl = item.href;
+						// Get anchor URL object using itemUrl (const)
+						const menuItemPath = new URL(itemUrl);
 
-							// Get anchor URL object using itemUrl (const)
-							const menuItemPath = new URL(itemUrl);
+						// Create correct path reference
+						const itemPathHref = menuItemPath.href;
+						const itemPathName = menuItemPath.pathname;
+						// const itemPathOrigin = menuItemPath.origin;
 
-							// Create correct path reference
-							const itemPathHref = menuItemPath.href;
-							const itemPathName = menuItemPath.pathname;
-							// const itemPathOrigin = menuItemPath.origin;
+						// Check if link has a page assigned
+						// I.e. doesn't have hash (#) at the end
+						if (!itemPathHref.endsWith('#')) {
+							item.href = `/${gitHubRepoName}${itemPathName}`
+						}
+					});
 
-							// Check if link has a page assigned
-							// I.e. doesn't have hash (#) at the end
-							if (!itemPathHref.endsWith('#')) {
-								item.href = `/${gitHubRepoName}${itemPathName}`
-							}
-						});
-
-						// Update HTML that will be in header
-						html = nodeList.outerHTML;
-					}
+					// Update HTML that will be in header
+					html = nodeList.outerHTML;
 				}
-
-				// Some things are only relevant on index page
-				// This is so that other pages can work unobstructed
-				// if (isIndex) {
-
-				// 	// Parse HTML text to DOM and get body content
-				// 	const nodeList = new DOMParser().parseFromString(html, 'text/html').body;
-
-				// 	if (componentTag === 'HEADER') {
-
-				// 		// Get logotype src
-				// 		const logotypeElement = nodeList.querySelector('.container #main-header-logotype img');
-				// 		const logotypeSource = logotypeElement.getAttribute('src');
-				// 		logotypeElement.src = `..${logotypeSource}`;
-
-				// 		console.log(logotypeElement);
-				// 	}
-
-				// }
-
-				// If HTML string is not false add it to element
-				if (html)
-					// el.insertAdjacentHTML(
-					// 	'afterbegin',
-					// 	html
-					// );
-					el.innerHTML = html;
-
-				// Keep source code clean
-				// Remove keyword attribute from element
-				el.removeAttribute(keyword);
 			}
 
-		} catch (err) {
-			console.log(err);
+			// If HTML string is not false add it to element
+			if (html) {
+				el.innerHTML = html;
+			}
+			// Keep source code clean
+			// Remove keyword attribute from element
+			el.removeAttribute(keyword);
 		}
-	})
+	}
+
+	// Now that all component elements are in place
+	// it's safe to continue with the rest of our scripts
+
+	// Create empty script element for menu toggler
+	const menuTogglerScriptEl = document.createElement('script');
+
+	// Insert menu toggler file in script source
+	menuTogglerScriptEl.src = !isGitHub ? `/js/menu-toggle.js` : `/${gitHubRepoName}/js/menu-toggle.js`;
+
+	// Append script to head (NOTE! NOT header element)
+	document.head.appendChild(menuTogglerScriptEl);
+
+	// Create empty script element for search
+	const searchScripEl = document.createElement('script');
+
+	// Insert search file in script source
+	searchScripEl.src = !isGitHub ? `/js/search.js` : `/${gitHubRepoName}/js/search.js`;
+
+	// Create empty script element for scroll to top
+	const scrollTopScripEl = document.createElement('script');
+
+	// Insert scroll to top file in script source
+	scrollTopScripEl.src = !isGitHub ? `/js/scroll-to-top.js` : `/${gitHubRepoName}/js/scroll-to-top.js`;
+
+	// Append scripts to body. Append, rather than prepend
+	// will add script at the end of body since this file
+	// must load last (otherwise the function can't find its elements
+	// since they don't exist yet)
+	document.body.appendChild(searchScripEl);
+	document.body.appendChild(scrollTopScripEl);
 }
+
 // Run function
 componentLoader();
